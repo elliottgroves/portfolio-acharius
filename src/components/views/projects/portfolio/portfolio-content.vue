6eef35b19855922949278e605ca0e7d3c83ff3c6
Elliott Groves <sevorge@gmail.com>
2019-10-30 23:33:46 -0400

    Portfolio next/previous.

--- src/components/views/projects/portfolio/portfolio-content.vue
+++ src/components/views/projects/portfolio/portfolio-content.vue
@@ -2,7 +2,17 @@
   <div class="project-content">
     <div class="content">
       <content-block title="My Portfolio" theme="three">
-        <div class="block-one full-width">
+        <div class="block-one">
+          <div class="next-project-footer">
+            <div class="next-project">
+              <img class="triangle" src="@/assets/images/triangles/triangles-three.svg">
+              <span @click="$router.push({ name: 'IdentityManagement' })">Previous Project</span>
+            </div>
+            <div class="next-project">
+              <img class="triangle" src="@/assets/images/triangles/triangles-three.svg">
+              <span @click="$router.push({ name: 'FoodDelivery' })">Next Project</span>
+            </div>
+          </div>
         </div>
       </content-block>
     </div>
@@ -20,3 +30,33 @@
   }
 </script>
 
+<style scoped lang="scss">
+.block-one {
+  width: 900px;
+  .next-project-footer {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin-top: 100px;
+    font-size: 37px;
+    text-transform: uppercase;
+    color: #fff;
+    text-shadow: 3px 3px #FD5F80;
+    font-weight: bold;
+    .next-project {
+      display: flex;
+      .triangle {
+        margin-right: 10px;
+        width: 70px;
+      }
+      span {
+        cursor: pointer;
+        transition: margin 0.2s ease;
+        &:hover {
+          margin: 0 -5px 0 5px;
+        }
+      }
+    }
+  }
+}
+</style>
